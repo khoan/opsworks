ace40374e0f600339adcbd58a43f44582b3c9a0a
khoa nguyen <huukhoanguyen@hotmail.com>
2015-03-16 16:26:41 +1100

    create SSL file first

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,19 +1,19 @@
-include_recipe 'java'
-include_recipe 'elasticsearch'
-include_recipe 'elasticsearch::proxy'
-include_recipe 'elasticsearch::aws'
-include_recipe 'elasticsearch::plugins'
-
 file node.elasticsearch[:nginx][:ssl][:key_file] do
   owner 'root'
   group 'root'
-  mode '0660'
+  mode '0440'
   content node.elasticsearch[:nginx][:ssl][:key]
 end
 
 file node.elasticsearch[:nginx][:ssl][:cert_file] do
   owner 'root'
   group 'root'
-  mode '0660'
+  mode '0440'
   content node.elasticsearch[:nginx][:ssl][:cert]
 end
+
+include_recipe 'java'
+include_recipe 'elasticsearch'
+include_recipe 'elasticsearch::aws'
+include_recipe 'elasticsearch::plugins'
+include_recipe 'elasticsearch::proxy'

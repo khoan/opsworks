5045e12a5a7cc170a1d02f4dfb6a84ad48d87de8
khoa nguyen <huukhoanguyen@hotmail.com>
2015-03-16 16:20:37 +1100

    assign SSL files to root

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,15 @@ include_recipe 'elasticsearch::aws'
 include_recipe 'elasticsearch::plugins'
 
 file node.elasticsearch[:nginx][:ssl][:key_file] do
+  owner 'root'
+  group 'root'
+  mode '0660'
   content node.elasticsearch[:nginx][:ssl][:key]
 end
 
 file node.elasticsearch[:nginx][:ssl][:cert_file] do
+  owner 'root'
+  group 'root'
+  mode '0660'
   content node.elasticsearch[:nginx][:ssl][:cert]
 end

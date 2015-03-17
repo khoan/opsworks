b88f74fe79d22a903e28e14d831c323d04347aba
khoa nguyen <huukhoanguyen@hotmail.com>
2015-03-17 11:13:57 +1100

    - update metadata.rb with salient data - bump version to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,19 @@
-name             "issue-elastic"
+name         'issue-elastic'
+version      '0.1.0'
+issues_url   'https://github.com/khoan/opsworks/issues'
+source_url   'https://github.com/khoan/opsworks'
+supports     'ubuntu', '= 14.04'
 
-maintainer       "khoa"
-maintainer_email "khoa@issue.by"
-license          "Apache"
-description      "Installs and configures elasticsearch"
+
+provides     'issue-elastic'
+
+depends      'java'
+depends      'elasticsearch'
+
+
+license          'MIT'
+description      'Installs and configures elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
 
-provides 'issue-elastic'
-
-depends 'java'
-depends 'elasticsearch'
+maintainer       'khoa'
+maintainer_email 'khoa@issue.by'

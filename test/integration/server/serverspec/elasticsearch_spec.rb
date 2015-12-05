29a90ffd0ca935c958bfee3c7c6b6692401651cc
khoa nguyen <huukhoanguyen@hotmail.com>
2015-12-06 00:25:58 +1100

    add some tests

---
+++ test/integration/server/serverspec/elasticsearch_spec.rb
@@ -0,0 +1,29 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe 'Elasticsearch' do
+  it 'is listening on port 9200'
+  it 'is listening on port 9300'
+  it 'is run as a service'
+
+  it 'is 1.7.3 version'
+
+  describe 'Plugin' do
+    it 'has aws cloud 2.7.1 plugin'
+    it 'has marvel 1.3.1 plugin'
+  end
+
+  describe 'Configuration' do
+    it 'exports node stats to monitoring cluster'
+    it 'exports node stats to monitoring cluster via https'
+    it 'clusters within elasticsearch group'
+    it 'supports CORS requests'
+
+    it 'stores data at /data/db/elasticsearch'
+    it 'stores log at /data/log/elasticsearch'
+
+    # TODO
+    # tuning JVM
+  end
+end

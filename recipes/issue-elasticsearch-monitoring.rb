93a9b310e613b63ffc987691575b45765ed42094
khoa nguyen <huukhoanguyen@hotmail.com>
2015-12-09 18:29:36 +1100

    spec out elasticsearch monitoring cluster

---
+++ recipes/issue-elasticsearch-monitoring.rb
@@ -0,0 +1,37 @@
+include_recipe 'apt'
+
+include_recipe 'java'
+
+elasticsearch_user 'elasticsearch'
+elasticsearch_install 'elasticsearch 1.7'
+
+elasticsearch_plugin 'marvel' do
+  url 'elasticsearch/marvel/1.3.1'
+end
+
+elasticsearch_configure 'elasticsearch' do
+  path_data(
+    package: '/data/db/elasticsearch',
+    tarball: '/data/db/elasticsearch'
+  )
+
+  path_logs(
+    package: '/data/log/elasticsearch',
+    tarball: '/data/log/elasticsearch'
+  )
+
+  configuration(
+    'cluster.name' => 'issue-elasticsearch',
+
+    'http.cors.enabled' => true,
+    'http.cors.allow-origin' => '/.*/',
+    'http.cors.allow-headers' => 'X-Requested-With, Content-Type, Content-Length, Authorization',
+    'http.cors.allow-credentials' => true,
+
+    'marvel.agent.enabled' => false
+  )
+end
+
+elasticsearch_service 'elasticsearch' do
+  service_actions([:enable, :start])
+end
--- test/integration/default/serverspec/localhost/elasticsearch_monitoring_spec.rb
+++ test/integration/default/serverspec/localhost/elasticsearch_monitoring_spec.rb
@@ -1,29 +1,79 @@
 require 'serverspec'
+require 'json'
 
 set :backend, :exec
 
-describe 'Elasticsearch monitoring' do
-  it 'is listening on port 9200'
-  it 'is listening on port 9300'
-  it 'is run as a service'
+describe 'Elasticsearch' do
+  # TODO
+  # tuning JVM
 
-  it 'is 1.7.3 version'
-
-  describe 'Plugin' do
-    it 'has aws cloud 2.7.1 plugin'
-    it 'has marvel 1.3.1 plugin'
+  describe port(9200) do
+    it { should be_listening }
   end
 
-  describe 'Configuration' do
-    it 'accepts stats data from elasticsearch cluster'
-    it 'clusters within elasticsearch group'
-    it 'supports CORS requests'
+  describe port(9300) do
+    it { should be_listening }
+  end
 
-    it 'stores data at /data/db/elasticsearch'
-    it 'stores log at /data/log/elasticsearch'
+  describe service('elasticsearch') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 
-    # TODO
-    # tuning JVM
+  describe command('curl localhost:9200') do
+    it 'is version 1.7.3' do
+      hash = JSON.parse(subject.stdout)
+      expect(hash['version']['number']).to eq '1.7.3'
+    end
+  end
+
+  # Plugins
+  describe command('curl localhost:9200/_nodes') do
+    let(:plugins) do
+      hash = JSON.parse(subject.stdout)
+      hash['nodes'].values.first['plugins']
+    end
+
+    it 'has marvel 1.3.1 plugin' do
+      expect(plugins).to satisfy {|plugins|
+        plugins.any? do |plugin|
+          plugin['name'] == 'marvel' && plugin['version'] == '1.3.1'
+        end
+      }
+    end
+  end
+
+  # cluster settings: discovery, CORS, data & log mount point
+  describe command('curl localhost:9200/_nodes') do
+    let(:settings) do
+      hash = JSON.parse(subject.stdout)
+      hash['nodes'].values.first['settings']
+    end
+
+    it 'supports CORS requests' do
+      expect(settings['http']).to include(
+        'cors' => {
+          'enabled' => 'true',
+          'allow-origin' => '/.*/',
+          'allow-headers' => 'X-Requested-With, Content-Type, Content-Length, Authorization',
+          'allow-credentials' => 'true',
+        }
+      )
+    end
+
+    it 'stores data at /data/db/elasticsearch' do
+      expect(settings['path']['data']).to eq '/data/db/elasticsearch'
+    end
+
+    it 'stores log at /data/log/elasticsearch' do
+      expect(settings['path']['logs']).to eq '/data/log/elasticsearch'
+    end
+  end
+
+  describe file('/etc/elasticsearch/elasticsearch.yml') do
+    it { should be_file }
+
+    # accepts stats data from elasticsearch cluster
+    its(:content) { should match %r{marvel.agent.enabled:\s+false} }
   end
 end
-

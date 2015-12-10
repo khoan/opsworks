253457060306d4305e5a027fdec4596c44c2b9de
khoa nguyen <huukhoanguyen@hotmail.com>
2015-12-11 01:33:53 +1100

    add more spec around gate

--- test/integration/elasticsearch_gate/serverspec/localhost/elasticsearch_gate_spec.rb
+++ test/integration/elasticsearch_gate/serverspec/localhost/elasticsearch_gate_spec.rb
@@ -12,9 +12,20 @@ describe 'Elasticsearch gate' do
     it { should be_listening }
   end
 
-  it 'authenticates via HTTP Basic'
-  it 'serves self signed SSL cert'
+  describe command('curl -k https://bam:bam@localhost') do
+    its(:stdout) { should match /502 Bad Gateway/ }
 
+    #it 'authenticates via HTTP Basic' do
+    #  hash = JSON.parse(subject.stdout)
+    #  expect(hash['status']).to eq 200
+    #end
+  end
+
+  # serves self signed SSL cert
+  describe command('curl https://bam:bam@localhost') do
+    its(:exit_status) { should eq 60 }
+  end
+  
   it 'proxies elasticsearch cluster'
   it 'proxies CORS headers to-and-fro elasticsearch cluster'
 
